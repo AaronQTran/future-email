8f96d660c92b7dc82fa1c57d0f08e56adeadf15d
Aaron Tran <dahpaladin123@gmail.com>
2024-06-06 22:00:17 -0400

    use states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const SendMessage = () => {
+  const[message, setMessage] = useState('');
+  const[email, setEmail] = useState('');
+  const[date, setDate] = useState('');
+
   return (
     <div className="text-right bg-amber-50 w-1/2">
       <h1 className="text-3xl font-bold underline">
-        f
+        fs
       </h1>
     </div>
   );
   
 }
 
-export default App;
+export default SendMessage;
